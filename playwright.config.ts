0893b5227ff2d6ad8006211b6ce5ecb44acc1da0
IMfaycel-web <ekramfaycel@gmail.com>
2024-02-15 01:15:47 +0500

    Initial commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
 
 
   workers: process.env.CI ? 4 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
+
   reporter: [["html", { outputFolder: "playwright-report", open: "never" }]],
   /* Shared settings for all the workspaces below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
