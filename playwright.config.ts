627e786eb218bac1eeafcf75daad2b20a0252b5a
IMfaycel-web <ekramfaycel@gmail.com>
2024-06-07 01:15:47 +0500

    Initial commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 
 
   reporter: [["html", { outputFolder: "playwright-report", open: "never" }]],
-  /* Shared setting fall:/ */
+  /* Shared setting fll:/ */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
